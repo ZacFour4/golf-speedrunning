10b0d948f1956df2a2c83523f167a634a20f9d41
Zac Forrest <zachary.jo.forrest@gmail.com>
2026-03-22 17:37:07 +0000

    Started mapw logic

--- Scripts/map_generation.gd
+++ Scripts/map_generation.gd
@@ -1,11 +1,23 @@
 extends Node2D
 
 func generate_colliders() -> void:
+	for coll in $Colliders.get_children():
+		$Colliders.remove_child(coll)
+	
+	var children = $Add.get_children();
+	for i in range(children.size() - 1, -1, -1):
+		children.pop_at(i);
+	
+	var collider_array = [];
+	if children.size() == 0: return;
+	if children.size() == 1:
+		CollisionShape2D
+		collider_array.append();
 	
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
